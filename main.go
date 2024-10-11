3266b79ca3a033a9ada2590b371850524a4faa2d
Nickolay Kirilman <nikolajkirilman@gmail.com>
2024-10-11 10:55:20 +0300

    add main.go

--- main.go
+++ main.go
@@ -7,9 +7,14 @@ import (
 
 func main() {
 	const IMTPower = 2
-	userHeight := 1.8
-	userKg := 100.0
+	var userHeight float64
+	var userKg float64
+	fmt.Println("_Калькулятор индекса массы тела_")
+	fmt.Println("Введите ваш рост в метрах: ")
 	fmt.Scan(&userHeight)
+	fmt.Println("Введите ваш вес в килограммах: ")
+	fmt.Scan(&userKg)
 	IMT := userKg / math.Pow(userHeight, IMTPower)
+	fmt.Println("Ваш индекс массы тела: ")
 	fmt.Println(IMT)
 }
